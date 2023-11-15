74f072e1001cd73e048cab4e004dfd7011f6b38d
Pierre <pierre.hamonic71@gmail.com>
2023-11-15 18:56:36 +0100

    Initialisation

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+    </head>
+
+    <body>
+        <!-- Entête -->
+        <header>
+        </header>
+
+        <!-- Contenu -->
+        <section>
+            <article>
+                <?php
+                    echo 'Hello World!';
+                ?>
+            </article>
+        </section>
+
+        <!-- Pied de page -->
+        <footer>
+        </footer>
+    </body>
+</html>
